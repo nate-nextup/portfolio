4125c3e10f17baf0bbf11d95abd28fd0986a3df8
nate-nextup <nathanmnicks03@icloud.com>
2019-07-16 13:56:03 -0700

    added search bar

--- Demo Day Brother/purecolor.js
+++ Demo Day Brother/purecolor.js
@@ -3,3 +3,35 @@ var images = ["https://ci3.googleusercontent.com/proxy/G5C455JL2Jz6Wc8khgHRNAkS6
 function loadImage(imageID) {
     document.write(images[imageID]);
 }
+function myFunction() {
+  // document.getElementById("myUL").style.display = "";
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+
+  input = document.getElementById('myInput');
+  // setting a variable filter equal to the input in our search bar
+  // and changing it to uppercase
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  //ul.style.display = "";
+  // creating an array with each li tag inside 
+  li = ul.getElementsByTagName('li');
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+      //console.log(li[i]);
+    // grabs a tag from each li tag  
+    a = li[i].getElementsByTagName("a")[0];
+    console.log(a);
+    // sets txtValue text of the a href tag 
+    txtValue = a.textContent || a.innerText;
+    //when search input matches one of the li 
+ 
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        ul.style.display = "";
+        li[i].style.display = "";
+    } else {
+       ul.style.display = "";
+      li[i].style.display = "none";
+    }
+  }
+}
